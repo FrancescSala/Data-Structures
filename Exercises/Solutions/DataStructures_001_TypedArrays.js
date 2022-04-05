2249f4971adc060822ab3c8b129b104ffd4a99b7
FrancescSala <fsala.valcarcel@gmail.com>
2022-04-05 21:19:41 +0200

    exercises

--- Exercises/Solutions/DataStructures_001_TypedArrays.js
+++ Exercises/Solutions/DataStructures_001_TypedArrays.js
@@ -1,6 +1,8 @@
 var buffer = new ArrayBuffer(64);
 var i32View = new Int32Array(buffer);
-//console.log(i32View);
+/*
+console.log(i32View);
 console.log(i32View.byteLength);
 console.log(i32View.length);
 console.log(i32View[0]);
+*/
