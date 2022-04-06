cbc16b815b6c40dea06afc2d06ea0eaa814edb1a
FrancescSala <fsala.valcarcel@gmail.com>
2022-04-06 07:35:08 +0200

    exercises

--- Exercises/Solutions/DataStructures_004_QueueClass.js
+++ Exercises/Solutions/DataStructures_004_QueueClass.js
@@ -12,12 +12,12 @@ class Queue {
     }
   
     dequeue() {
-      if (this.isEmpty) throw Error('Empty queue');
+      if (this.isEmpty()) throw Error('Empty queue');
       return this.collection.shift();
     }
   
     front() {
-      if (this.isEmpty) throw Error('Empty queue');
+      if (this.isEmpty()) throw Error('Empty queue');
       return this.collection[0];
     }
   
@@ -30,3 +30,41 @@ class Queue {
     }
   }
   
+
+  /*
+  // Alternatively it could be done following the code started in the freeCodecCamp for this exercise:
+  // with a funtion. See below. But that is not a class:
+
+  function Queue() {
+    var collection = [];
+    this.print = function() {
+      console.log(collection);
+    };
+    // Only change code below this line
+  
+    this.enqueue = function(item) {
+      collection.push(item);
+    };
+  
+    this.dequeue = function() {
+      if (this.isEmpty()) throw Error('Empty queue');
+      return collection.shift();
+    };
+  
+    this.front = function() {
+      if (this.isEmpty()) throw Error('Empty queue');
+      return collection[0];
+    };
+  
+    this.size = function() {
+      return collection.length;
+    };
+  
+    this.isEmpty = function() {
+      return collection.length === 0;
+    };
+  
+    // Only change code above this line
+  }
+
+  */
