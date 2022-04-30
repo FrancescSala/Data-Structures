086d24ac0e3c32d99f232ba2e70a85bc9e887d48
FrancescSala <fsala.valcarcel@gmail.com>
2022-05-01 01:08:01 +0200

    exercises

--- Exercises/Solutions/DataStructures_004_CreateAQueueClass.js
+++ Exercises/Solutions/DataStructures_004_CreateAQueueClass.js
@@ -1,41 +1,4 @@
-class Queue {
-    constructor() {
-      this.collection = [];
-    }
-  
-    print() {
-      console.log(this.collection);
-    }
-    
-    enqueue(item) {
-      this.collection.push(item);
-    }
-  
-    dequeue() {
-      if (this.isEmpty()) throw Error('Empty queue');
-      return this.collection.shift();
-    }
-  
-    front() {
-      if (this.isEmpty()) throw Error('Empty queue');
-      return this.collection[0];
-    }
-  
-    size() {
-      return this.collection.length;
-    }
-  
-    isEmpty() {
-      return this.collection.length === 0;
-    }
-  }
-  
-
-  /*
-  // Alternatively it could be done following the code started in the freeCodecCamp for this exercise:
-  // with a funtion. See below. But that is not a class:
-
-  function Queue() {
+function Queue() {
     var collection = [];
     this.print = function() {
       console.log(collection);
@@ -67,4 +30,37 @@ class Queue {
     // Only change code above this line
   }
 
-  */
+/*
+// I would have preferred like this:
+class Queue {
+    constructor() {
+      this.collection = [];
+    }
+  
+    print() {
+      console.log(this.collection);
+    }
+    
+    enqueue(item) {
+      this.collection.push(item);
+    }
+  
+    dequeue() {
+      if (this.isEmpty()) throw Error('Empty queue');
+      return this.collection.shift();
+    }
+  
+    front() {
+      if (this.isEmpty()) throw Error('Empty queue');
+      return this.collection[0];
+    }
+  
+    size() {
+      return this.collection.length;
+    }
+  
+    isEmpty() {
+      return this.collection.length === 0;
+    }
+  }
+*/
