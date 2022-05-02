3e18692f43094fd152ed5f6557469057925e66a8
FrancescSala <fsala.valcarcel@gmail.com>
2022-05-02 20:33:54 +0200

    exercises

--- Exercises/Solutions/DataStructures_006_CreateACircularQueue.js
+++ Exercises/Solutions/DataStructures_006_CreateACircularQueue.js
@@ -1,4 +1,45 @@
 class CircularQueue {
+  constructor(size) {
+
+    this.queue = [];
+    this.read = 0;
+    this.write = 0;
+    this.max = size - 1;
+
+    while (size > 0) {
+      this.queue.push(null);
+      size--;
+    }
+  }
+
+  print() {
+    return this.queue;
+  }
+
+  enqueue(item) {
+    // Only change code below this line
+    if (this.queue[this.write] != null) return null;
+    this.queue[this.write] = item;
+    this.write = (this.write + 1) % (this.max + 1);
+    return item;
+    // Only change code above this line
+  }
+
+  dequeue() {
+    // Only change code below this line
+    let v = this.queue[this.read];
+    if (v === null) return null;
+    this.queue[this.read] = null;
+    this.read = (this.read + 1) %  (this.max + 1);
+    return v;
+    // Only change code above this line
+  }
+}
+
+/*
+// Personally I prefer like this
+
+class CircularQueue {
     constructor(size) {
         if (isNaN(size)) throw Error('A number was expected as argument');
         if (size < 0) throw Error('size should be a positive number');
@@ -27,16 +68,5 @@ class CircularQueue {
     }
   }
 
-/*
-let cq = new CircularQueue(5);
-cq.enqueue('a');
-cq.enqueue('b');
-cq.enqueue('c');
-console.log(cq.dequeue());
-console.log(cq.dequeue());
-console.log(cq.dequeue());
-cq.enqueue('d');
-cq.enqueue('e');
-cq.enqueue('f');
-console.log(cq);
 */
+
