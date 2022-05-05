7d70d0d7d35bfc035c48be35fcd6358fbcaee4b3
FrancescSala <fsala.valcarcel@gmail.com>
2022-05-05 07:36:56 +0200

    exercises

--- Exercises/Solutions/DataStructures_027_AddANewElementToABinarySearchTree.js
+++ Exercises/Solutions/DataStructures_027_AddANewElementToABinarySearchTree.js
@@ -19,41 +19,15 @@ function BinarySearchTree() {
     // Note two facts:
     // - the exact shape of the tree depends on the order the elements are added to it
     // - given an order of the additions, there is no a single strategy to locate the elems: resulting in more or less balanced tree
+    // - I AM GOING SIMPLE and will add new values as leafs of the tree 
+    // - (a smarter strategy would have been trying to keep tree balanced: that would improve search time in the tree)
     while (n !== null) {
-        if (p !== null && n!== null && p.value <= elem && elem <= n.value) break;
-        if (p !== null && n!== null && p.value >= elem && elem >= n.value) break;
-        if (n !== null && n.value === elem) break;
         p = n;
+        if (n.value === elem) return null;  // not very elegant
         n = (n.value > elem) ? n.left : n.right;
     }
-    // There are 4 cases now:
-    // 1. Tree already contains elem
-    // 2. n === null, elem has to be inserted as a leaf in the tree; as a child of p, left or right child has to be seen
-    // 3. p < elem < n; elem must be inserted between p and n (right child of p and n will become left child of elem)
-    // 4. p > elem > n; elem must be inserted between p and n (left child of p and n will become right child of elem)
-
-    // Case 1. Tree already contains elem
-    if (n !== null && n.value === elem) return null;
-
-    // Case 2. n === null, elem has to be inserted as a leaf in the tree; as a child of p, left or right child has to be seen
-    if (n === null) {
-      if (p.value < elem) p.right = o;
-      else p.left = o;
-      return;
-    }
-
-    // Case 3. p < elem < n; elem must be inserted between p and n (right child of p and n will become left child of elem)
-    if (p !== null && n!== null && p.value <= elem && elem <= n.value) {
-       p.right = o;
-       o.right = n;
-       return;
-    }
-    // Case 4. p > elem > n; elem must be inserted between p and n (left child of p and n will become right child of elem)
-    if (p !== null && n!== null && p.value >= elem && elem >= n.value) {
-      p.left = o;
-      o.left = n;
-    }
-  }; 
+    if (p.value > elem) p.left = o;
+    else p.right = o;
   // Only change code above this line
+  }
 }
-
--- Exercises/Solutions/DataStructures_028_FindTheMinimumAndMaximumValueInABinarySearchTree.js
+++ Exercises/Solutions/DataStructures_028_FindTheMinimumAndMaximumValueInABinarySearchTree.js
@@ -19,41 +19,16 @@ function BinarySearchTree() {
     // Note two facts:
     // - the exact shape of the tree depends on the order the elements are added to it
     // - given an order of the additions, there is no a single strategy to locate the elems: resulting in more or less balanced tree
+    // - I AM GOING SIMPLE and will add new values as leafs of the tree 
+    // - (a smarter strategy would have been trying to keep tree balanced: that would improve search time in the tree)
     while (n !== null) {
-        if (p !== null && n!== null && p.value <= elem && elem <= n.value) break;
-        if (p !== null && n!== null && p.value >= elem && elem >= n.value) break;
-        if (n !== null && n.value === elem) break;
         p = n;
+        if (n.value === elem) return null;  // not very elegant
         n = (n.value > elem) ? n.left : n.right;
     }
-    // There are 4 cases now:
-    // 1. Tree already contains elem
-    // 2. n === null, elem has to be inserted as a leaf in the tree; as a child of p, left or right child has to be seen
-    // 3. p < elem < n; elem must be inserted between p and n (right child of p and n will become left child of elem)
-    // 4. p > elem > n; elem must be inserted between p and n (left child of p and n will become right child of elem)
-
-    // Case 1. Tree already contains elem
-    if (n !== null && n.value === elem) return null;
-
-    // Case 2. n === null, elem has to be inserted as a leaf in the tree; as a child of p, left or right child has to be seen
-    if (n === null) {
-      if (p.value < elem) p.right = o;
-      else p.left = o;
-      return;
-    }
-
-    // Case 3. p < elem < n; elem must be inserted between p and n (right child of p and n will become left child of elem)
-    if (p !== null && n!== null && p.value <= elem && elem <= n.value) {
-       p.right = o;
-       o.right = n;
-       return;
-    }
-    // Case 4. p > elem > n; elem must be inserted between p and n (left child of p and n will become right child of elem)
-    if (p !== null && n!== null && p.value >= elem && elem >= n.value) {
-      p.left = o;
-      o.left = n;
-    }
-  }; 
+    if (p.value > elem) p.left = o;
+    else p.right = o;
+  };
   
   this.findMin = function() {
     if (this.root === null) return null;
@@ -70,13 +45,3 @@ function BinarySearchTree() {
   };    
   // Only change code above this line
 }
-
-let t = new BinarySearchTree();
-t.add(8);
-t.add(3);
-t.add(1);
-t.add(10);
-t.add(6);
-t.add(13);
-t.add(14);
-displayTree(t);
